73cb0d0bd0b1e2da9f08590ddd211e9a605afcd0
getaudio2 <paugermaisar@hotmail.es>
2023-03-22 16:17:54 +0100

    Added CategoriasViewComponent for a list view of Categorias

---
+++ ShoppingCart/Infrastructure/Components/CategoriasViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCart.Infrastructure.Components
+{
+    public class CategoriasViewComponent:ViewComponent
+    {
+
+        private readonly DataContext _context;
+
+        public CategoriasViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categorias.ToListAsync());
+    }
+}
