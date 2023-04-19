2499cc8be21903b13397c868a07a89ddd3a0c9ec
getaudio2 <paugermaisar@hotmail.es>
2023-04-19 16:22:10 +0200

    Ver Carrito now redirects to CarroCompra Index page

--- ShoppingCart/Controllers/CarroCompraController.cs
+++ ShoppingCart/Controllers/CarroCompraController.cs
@@ -15,7 +15,7 @@ namespace ShoppingCart.Controllers
         }
         public IActionResult Index()
         {
-            List<ItemCarrito> carrito = HttpContext.Session.GetJson<List<ItemCarrito>>("Carrito");
+            List<ItemCarrito> carrito = HttpContext.Session.GetJson<List<ItemCarrito>>("Carrito") ?? new List<ItemCarrito>();
 
             CarroCompraViewModel carroCompraVM = new()
             {
@@ -23,6 +23,29 @@ namespace ShoppingCart.Controllers
                 GranTotal = carrito.Sum(x => x.Cantidad * x.Precio)
             };
             return View(carroCompraVM);
+        }
+        public async Task<IActionResult> Añadir(long id)
+        {
+            Producto producto = await _context.Productos.FindAsync(id);
+
+            List<ItemCarrito> carrito = HttpContext.Session.GetJson<List<ItemCarrito>>("Carrito") ?? new List<ItemCarrito>();
+
+            ItemCarrito itemCarrito = carrito.Where(c => c.ProductoId == id).FirstOrDefault();
+
+            if (itemCarrito == null)
+            {
+                carrito.Add(new ItemCarrito(producto));
+            }
+            else
+            {
+                itemCarrito.Cantidad += 1;
+            }
+
+            HttpContext.Session.SetJson("Carrito", carrito);
+
+            TempData["Success"] = "Producto añadido con éxito!";
+
+            return Redirect(Request.Headers["Referer"].ToString());
         }
     }
 }
