e643fc010edb0e1799a19d6a17be684932bc5ce0
getaudio2 <paugermaisar@hotmail.es>
2023-04-19 15:11:57 +0200

    Added CarritoVM

--- ShoppingCart/Infrastructure/Components/CarritoViewComponent.cs
+++ ShoppingCart/Infrastructure/Components/CarritoViewComponent.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
+using ShoppingCart.Models.ViewModels;
 
 namespace ShoppingCart.Infrastructure.Components
 {
     public class CarritoViewComponent : ViewComponent
     {
-        
+        public IViewComponentResult Invoke()
+        {
+            List<ItemCarrito> carrito = HttpContext.Session.GetJson<List<ItemCarrito>>("Carrito");
+            CarritoViewModel carritoVM;
+
+            if (carrito == null || carrito.Count == 0)
+            {
+                carritoVM = null;
+            }
+            else 
+            {
+                carritoVM = new()
+                {
+                    CantidadItems = carrito.Sum(x => x.Cantidad),
+                    PrecioTotal = carrito.Sum(x => x.Cantidad * x.Precio)
+                };
+            }
+            
+            return View(carritoVM);
+        }
     }
 }
