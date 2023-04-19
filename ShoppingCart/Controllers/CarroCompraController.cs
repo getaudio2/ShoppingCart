0172749b750a3f9c85773983c74b3e500016e963
getaudio2 <paugermaisar@hotmail.es>
2023-04-19 15:50:48 +0200

    Added Controller and View for Carro Compra

---
+++ ShoppingCart/Controllers/CarroCompraController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Infrastructure;
+using ShoppingCart.Models;
+using ShoppingCart.Models.ViewModels;
+
+namespace ShoppingCart.Controllers
+{
+    public class CarroCompraController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CarroCompraController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<ItemCarrito> carrito = HttpContext.Session.GetJson<List<ItemCarrito>>("Carrito");
+
+            CarroCompraViewModel carroCompraVM = new()
+            {
+                ItemsCarrito = carrito,
+                GranTotal = carrito.Sum(x => x.Cantidad * x.Precio)
+            };
+            return View(carroCompraVM);
+        }
+    }
+}
---
+++ ShoppingCart/Models/ViewModels/CarroCompraViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingCart.Models.ViewModels
+{
+    public class CarroCompraViewModel
+    {
+        public List<ItemCarrito> ItemsCarrito { get; set; }
+        public decimal GranTotal { get; set; }
+    }
+}
